095296520158381b3ecd08f933b99b1490a841b6
DanilMorin <DanilMorin>
2026-03-10 18:12:51 +0300

    Add new css files and update enqueue.php

--- front-page.php
+++ front-page.php
@@ -4,8 +4,17 @@
  */
 ?>
 <?php get_header(); ?>
-<section>
-    <h1>Главная страница</h1>
-</section>
-<?php get_footer();?>
+<section class="hero">
+    <div class="hero__container">
+        <div class="hero_titles">
+            <h1 class="text-title">Полный спектр услуг в строительстве электроэнергетической инфраструктуры</h1>
+            <p>ЭНЕРГОСТРОЙ - Энергия в каждом решении, надежность в каждом проекте</p>
+            <a href="/">Подробнее о компании</a>
+        </div>
+        <div class="hero__img">
+             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/main.png"  alt="ЭНЕРГОСТРОЙ - Энергия в каждом решении, надежность в каждом проекте">
+        </div>
+    </div>
 
+</section>
+<?php get_footer(); ?>
