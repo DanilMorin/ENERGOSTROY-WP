7392b0c621ff17d43a11624f729cc6cec2309f80
DanilMorin <DanilMorin>
2026-04-29 20:47:04 +0300

    Add new structure and new photo

--- front-page.php
+++ front-page.php
@@ -4,22 +4,32 @@
  */
 ?>
 
-  <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/main.png" alt="ЭНЕРГОСТРОЙ - Энергия в каждом решении, надежность в каждом проекте"> -->
+<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/main.png" alt="ЭНЕРГОСТРОЙ - Энергия в каждом решении, надежность в каждом проекте"> -->
 <?php get_header(); ?>
 
 
 <main class="site-main">
 
     <section class="hero">
-        
+        <div class="hero__image">
+            <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/mainMob.png" media="(max-width: 500px)">
+                <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/main.png" media="(max-width: 1024px)">
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/main.png"
+                    alt="ЭНЕРГОСТРОЙ"
+                    class="hero__img">
+            </picture>
+        </div>
+
     </section>
 
     <section class="about" id="about">
-        
+
     </section>
 
     <section class="partners">
-        
+
     </section>
 
 </main>
