8af5d839ae7068a8bdc0547f30f426b825846f50
DanilMorin <DanilMorin>
2026-05-07 14:09:33 +0300

    Add block about

--- front-page.php
+++ front-page.php
@@ -95,38 +95,54 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bell.svg" alt="">
                     </div>
                     <div class="card__content">
-                        <h3 >Заявка</h3>
+                        <h3>Заявка</h3>
                         <p class="text-card">Обсуждение задачи</p>
                     </div>
                 </div>
-                 <div class="card">
+                <div class="card">
                     <div class="card__image">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/wallet.svg" alt="">
                     </div>
                     <div class="card__content">
-                        <h3 >Расчёт стоимости</h3>
+                        <h3>Расчёт стоимости</h3>
                         <p class="text-card">Фиксируем стоимость до начала работ</p>
                     </div>
                 </div>
-                  <div class="card">
+                <div class="card">
                     <div class="card__image">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/wallet.svg" alt="">
                     </div>
                     <div class="card__content">
-                        <h3 >Договор</h3>
+                        <h3>Договор</h3>
                         <p class="text-card">Работаем по договору. Выполняем работы под ключ, контролируя сроки и качество</p>
                     </div>
                 </div>
-                 <div class="card">
+                <div class="card">
                     <div class="card__image">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/wallet.svg" alt="">
                     </div>
                     <div class="card__content">
-                        <h3 >Выполнение работ</h3>
-                        <p class="text-card">Используем проверенные решения и оборудование. Работаем  с поставщиками и подрядчиками</p>
+                        <h3>Выполнение работ</h3>
+                        <p class="text-card">Используем проверенные решения и оборудование. Работаем с поставщиками и подрядчиками</p>
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+    <section class="about">
+        <div class="container">
+            <h2 class="text-title-second">ЭНЕРГОСТРОЙ</h2>
+            <ul class="custom-list">
+                <li class="text-base">
+                    Наша команда объединяет высококлассных специалистов с многолетним опытом в строительстве и реконструкции энергетических объектов. Мы успешно работаем над масштабными проектами, обеспечивая надежность и качество на каждом этапе
+                </li>
+                <li class="text-base">
+                    Для достижения высоких результатов мы непрерывно внедряем передовые технологии и расширяем наши технические возможности. Постоянное развитие и накопление знаний позволяют нам решать самые сложные задачи и находить эффективные решения для наших клиентов
+                </li>
+                <li class="text-base">
+                    Мы гордимся нашими профессионалами, чей опыт и экспертиза являются основой успеха компании. Вклад каждого сотрудника помогает нам уверенно расти, осваивать новые рынки и открывать новые горизонты в сфере энергетического строительства
+                </li>
+            </ul>
         </div>
     </section>
 
