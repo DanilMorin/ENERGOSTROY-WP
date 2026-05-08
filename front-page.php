134e3f435047ff7983dd660cb162744fbe9f06f6
DanilMorin <DanilMorin>
2026-05-08 15:43:24 +0300

    Clean code

--- front-page.php
+++ front-page.php
@@ -3,11 +3,7 @@
  Template Name: Main
  */
 ?>
-
-<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/main.png" alt="ЭНЕРГОСТРОЙ - Энергия в каждом решении, надежность в каждом проекте"> -->
 <?php get_header(); ?>
-
-
 <main class="site-main">
 
     <section class="hero">
@@ -177,5 +173,4 @@
         </div>
     </section>
 </main>
-
 <?php get_footer(); ?>
