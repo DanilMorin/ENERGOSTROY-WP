9d1ff1f829ea00c3f5e5bed00e5049e2be8091e0
DanilMorin <DanilMorin>
2026-05-11 18:16:26 +0300

    Update readme and footer

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+ <?php
+    $phone = get_theme_mod('energostroy_phone', '+7 (4742) 71-01-56');
+    $email = get_theme_mod('energostroy_email', 'energo_stroy48@bk.ru');
+
+    $phone_href = preg_replace('/[^0-9+]/', '', $phone);
+    ?>
+
 <footer class="footer">
     <div class="container">
         <div class="footer__inner">
@@ -14,14 +21,22 @@
             </div>
 
             <div class="footer__column footer__column--center">
-                <a  href="<?php echo esc_url( home_url('/') ); ?>">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
                     <?php echo date('Y'); ?> © ЭНЕРГОСТРОЙ
                 </a>
             </div>
 
             <div class="footer__column footer__column--contacts">
-                <a href="<?php echo esc_url('tel:+74742710156'); ?>" class="text-link">+7 (4742) 71-01-56</a>
-                <a href="<?php echo esc_url('mailto:enerenergo_stroy48@bk.ru'); ?>" class="text-link">enerenergo_stroy48@bk.ru</a>
+                <?php if ($phone) : ?>
+                    <a class="header__contact-link" href="tel:<?php echo esc_attr($phone_href); ?>">
+                        <?php echo esc_html($phone); ?>
+                    </a>
+                <?php endif ?>
+                <?php if ($email) : ?>
+                    <a class="header__contact-link" href="mailto:<?php echo esc_attr($email); ?>">
+                        <?php echo esc_html($email); ?>
+                    </a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
